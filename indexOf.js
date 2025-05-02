55c0db10acb0544821e78b90dde09e8e88cfdff6
Alejandro D Leon <alejandroperez9474@gmail.com>
2025-05-02 02:26:55 -0600

    Problema resuelto

---
+++ indexOf.js
@@ -0,0 +1,31 @@
+let haystack = 'hello world';
+let needle = 'o'
+
+let index = myIndexOf(haystack, needle);
+
+function myIndexOf(haystack, needle){
+
+    if(needle.length > haystack.length) return -1;
+
+    for(let i = 0; i <= haystack.length - needle.length; i++){
+
+        let find = true;
+
+        for(let j = 0; j< needle.length; j++){
+            if(haystack[i+j] !== needle[j]){
+                find = false;
+                break;
+            }
+        }
+
+        if(find){
+            return i;
+        }
+
+    }
+
+    return -1;
+}
+
+console.log(index);
+
